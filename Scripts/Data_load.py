d95d0230f3e0e459eb400b240e75bf4c2803e611
abreham <aynuyeabresh@gmail.com>
2021-09-24 06:53:09 -0300

    Feat: Working on Data load

--- Scripts/Data_load.py
+++ Scripts/Data_load.py
@@ -3,14 +3,13 @@ from sqlalchemy import create_engine, types
 import mysql.connector as mysql
 import pandas as pd
 
-from mysql.connector import MySQLConnection, Error
 
 my_data = "MyDatabase"
 
 class data_warehouse:
     def __init__(self, Host:str, user:str,my_password: str) -> None:
-        self.conn = self.connect(Host, user, my_password)
-        self.cursor = self.connect(Host, user, my_password)
+        self.conn = self.conn_database(Host, user, my_password)
+        self.cursor = self.conn_database(Host, user, my_password)
         pass
     def conn_database(self, Host: str, user: str, my_password: str, dbName: str = "MyDatabase"):
         
@@ -18,25 +17,40 @@ class data_warehouse:
             conn = mysql.connect(host=Host, 
                                 user=user, 
                                 password=my_password,
-                                auth_plugin='mysql_native_password',
+                                #auth_plugin='mysql_native_password',
                                 database=dbName, 
                                 buffered=True)
             return conn, conn.cursor()
 
         except Exception as ex:
-            conn = mysql.connect(host=Host, user=user, password=my_password, buffered=True)
+            conn = mysql.connect(host=Host, 
+                                 user=user,
+                                 #auth_plugin='mysql_native_password', 
+                                 password=my_password, 
+                                 buffered=True)
             cursor = conn.cursor() 
             sql = f"CREATE DATABASE {my_data}"
             cursor.execute(sql)
 
             return conn, cursor
     
-    def insertData(self, dbName: str,  host: str, user: str, password: str, tableName: str, csv_file: str):
+    def load_Data(self, dbName: str,  host: str, user: str, password: str, tableName: str, csv_file: str):
         engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{dbName}')
+        conn = mysql.connect(host=host, 
+                                user=user, 
+                                password=password,
+                                #auth_plugin='mysql_native_password',
+                                database=dbName, 
+                                buffered=True)
 
-        df = pd.read_csv(csv_file, sep=',', quotechar='\'', encoding='utf8')
-        df.to_sql(tableName, con=engine, index=False, if_exists='append')
+        try:
+            dataframe = pd.read_csv(csv_file, sep=',', quotechar='\'', encoding='utf8')
+            dataframe.to_sql(tableName, con=engine, index=False, if_exists='append')
+            print("Data Inserted Successfully")
+        except Exception as e:
+            conn.rollback()
+            print("Error: ", e)
 if __name__ == "__main__":
     TABLE_NAME = "sample"
-    a = data_warehouse(Host = "localhost", user = "root", my_password = "")
-    a.insertData(my_data , "localhost", "root", "", TABLE_NAME, "./I80_sample.txt" )
+    a = data_warehouse(Host = "localhost", user = "root", my_password = "Abrilow@13")
+    a.load_Data(my_data , "localhost", "root", "Abrilow@13", TABLE_NAME, "./I80_sample.txt" )
