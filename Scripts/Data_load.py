030aeff89bf297509d8ed7a3fb46152a5fbc60aa
abreham <aynuyeabresh@gmail.com>
2021-09-24 02:24:54 -0300

    feat: load Data

--- Scripts/Data_load.py
+++ Scripts/Data_load.py
@@ -1,1 +1,24 @@
-print("Loading Data")
+from mysql.connector import (connection)
+
+#establishing the connection
+conn = connection.MySQLConnection(user='root',
+                                  password='Abrilow@13', 
+                                  host='127.0.0.1'
+                                )
+cursor = conn.cursor()
+#Droping database MYDATABASE if already exists.
+cursor.execute("DROP database IF EXISTS MyDatabase")
+
+#Preparing query to create a database
+sql = "CREATE database MYDATABASE";
+
+#Creating a database
+cursor.execute(sql)
+
+#Retrieving the list of databases
+# print("List of databases: ")
+# cursor.execute("SHOW DATABASES")
+# print(cursor.fetchall())
+
+#Closing the connection
+conn.close()
